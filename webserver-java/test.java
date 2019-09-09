99a3c032e315d13521f117b4109e4603fd4b6c0d
Caitlyn Box <caitlynbox@Caitlyns-MacBook-Pro.local>
2019-09-09 13:35:41 -0500

    Add test file for java webserver and rename random number generator

---
+++ webserver-java/test.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import random_generator;
+
+public int numTests = 1000;
+
+public static void main(String[] args)
+{
+    ArrayList uniqueSamples = new ArrayList();
+
+    for (int i = 0; i < numTests; i++)
+    {
+	    
+    }
+}
